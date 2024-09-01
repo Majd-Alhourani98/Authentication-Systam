2a24e8390cce7bf7be7c502d1d1099133e7664a1
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-01 11:33:36 +0300

    initial project setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = require('./app');
+
+// Load Environment Variables
+dotenv.config();
+
+// Datebase connection
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(conn => console.log('CONNECTED TO DATABASE'))
+  .catch(err => console.log('NOT CONNTECTED TO DATABASE', err));
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
